7cdb0c4af0433dde2a18308d7fe437f2fb61483b
Eden Federman <eden@keyval.dev>
2023-06-12 10:54:37 +0300

    handle mount of bpffs

--- pkg/instrumentors/bpffs/bpfsfs.go
+++ pkg/instrumentors/bpffs/bpfsfs.go
@@ -18,6 +18,8 @@ import (
 	"fmt"
 	"os"
 
+	"go.opentelemetry.io/auto/pkg/log"
+
 	"golang.org/x/sys/unix"
 
 	"go.opentelemetry.io/auto/pkg/process"
@@ -31,22 +33,28 @@ func PathForTargetApplication(target *process.TargetDetails) string {
 }
 
 func Mount(target *process.TargetDetails) error {
-	_, err := os.Stat(bpfFsPath)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return err
-		}
-
+	if !isBPFFSMounted() {
 		// Directory does not exist, create it and mount
 		if err := os.MkdirAll(bpfFsPath, 0755); err != nil {
 			return err
 		}
 
-		err = unix.Mount(bpfFsPath, bpfFsPath, "bpf", 0, "")
+		err := unix.Mount(bpfFsPath, bpfFsPath, "bpf", 0, "")
 		if err != nil {
 			return err
 		}
 	}
 
 	return os.Mkdir(PathForTargetApplication(target), 0755)
+}
+
+func isBPFFSMounted() bool {
+	var stat unix.Statfs_t
+	err := unix.Statfs(bpfFsPath, &stat)
+	if err != nil {
+		log.Logger.Error(err, "failed to statfs bpf filesystem")
+		return false
+	}
+
+	return stat.Type == unix.BPF_FS_MAGIC
 }
